57e0df53f8e911536d20cdbd56b19f5db684c48d
Liyang HU <git@liyang.hu>
2013-02-07 00:58:35 +0900

    Data.Thyme.Format: fix 0-padding of negative years

--- src/Data/Thyme/Format/Internal.hs
+++ src/Data/Thyme/Format/Internal.hs
@@ -53,11 +53,12 @@ shows03 n
 
 {-# INLINE shows04 #-}
 shows04 :: Int -> String -> String
-shows04 n
-    | n < 10 = (++) "000" . shows n
-    | n < 100 = (++) "00" . shows n
-    | n < 1000 = (++) "0" . shows n
-    | otherwise = shows n
+shows04 n@(abs -> u)
+    | u < 10 = neg . (++) "000" . shows n
+    | u < 100 = neg . (++) "00" . shows n
+    | u < 1000 = neg . (++) "0" . shows n
+    | otherwise = neg . shows u
+    where neg = if n < 0 then (:) '-' else id
 
 {-# INLINE fills06 #-}
 fills06 :: Int64 -> ShowS
