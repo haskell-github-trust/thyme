41d5cc7f832eb588577b0c99f442a9faaa8a02fa
Liyang HU <git@liyang.hu>
2013-03-04 23:07:02 +0900

    Data.Thyme.LocalTime: getCurrentTimeZone copypasta

--- src/Data/Thyme/LocalTime/TimeZone.hs
+++ src/Data/Thyme/LocalTime/TimeZone.hs
@@ -6,19 +6,23 @@ module Data.Thyme.LocalTime.TimeZone (
     , T.minutesToTimeZone
     , T.hoursToTimeZone
     , T.utc
-    , T.getCurrentTimeZone
     , module Data.Thyme.LocalTime.TimeZone
     ) where
 
 import Prelude
 import Control.Lens
 import Data.Thyme.Calendar
-import Data.Thyme.Clock.Internal
+import Data.Thyme.Clock
 import qualified Data.Time as T
 
+{-# INLINEABLE getTimeZone #-}
 getTimeZone :: UTCTime -> IO T.TimeZone
 getTimeZone time = T.getTimeZone (T.UTCTime day dayTime) where
     day = T.ModifiedJulianDay (fromIntegral mjd)
     dayTime = fromRational (simply view seconds dt)
     UTCTime (ModifiedJulianDay mjd) dt = view utcTime time
+
+{-# INLINE getCurrentTimeZone #-}
+getCurrentTimeZone :: IO T.TimeZone
+getCurrentTimeZone = getCurrentTime >>= getTimeZone
 
