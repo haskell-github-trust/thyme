e0239b51b6dbacefd5ac1e327ac7d0a223ab2c70
Liyang HU <git@liyang.hu>
2013-09-21 18:36:52 +0900

    Data.Thyme.Clock: documentation

--- src/Data/Thyme/Clock.hs
+++ src/Data/Thyme/Clock.hs
@@ -1,4 +1,11 @@
--- | 'Num', 'Real', 'Fractional' and 'RealFrac' instances for 'DiffTime' and
+-- | Types and functions for
+-- <http://en.wikipedia.org/wiki/Coordinated_Universal_Time UTC> and
+-- <http://en.wikipedia.org/wiki/Universal_Time#Versions UT1>.
+--
+-- If you don't care about leap seconds, keep to 'UTCTime' and
+-- 'NominalDiffTime' for your clock calculations, and you'll be fine.
+--
+-- 'Num', 'Real', 'Fractional' and 'RealFrac' instances for 'DiffTime' and
 -- 'NominalDiffTime' are only available by importing "Data.Thyme.Time". In
 -- their stead are instances of 'Data.AdditiveGroup.AdditiveGroup',
 -- 'Data.Basis.HasBasis' and 'Data.VectorSpace.VectorSpace', with
@@ -43,6 +50,7 @@ import Control.Lens
 import Data.Thyme.Clock.Internal
 import Data.Thyme.Clock.POSIX
 
+-- | Get the current UTC time from the system clock.
 getCurrentTime :: IO UTCTime
 getCurrentTime = fmap (review posixTime) getPOSIXTime
 
--- src/Data/Thyme/Clock/Internal.hs
+++ src/Data/Thyme/Clock/Internal.hs
@@ -35,22 +35,23 @@ import Text.ParserCombinators.ReadP (char)
 import Text.Read (readPrec)
 #endif
 
--- | Time differences, encompassing both 'DiffTime' and 'NominalDiffTime'.
+-- | Time intervals, encompassing both 'DiffTime' and 'NominalDiffTime'.
 --
--- FIXME: still affected by <http://hackage.haskell.org/trac/ghc/ticket/7611>?
+-- [@Issues@] Still affected by
+-- <http://hackage.haskell.org/trac/ghc/ticket/7611>?
 class (HasBasis t, Basis t ~ (), Scalar t ~ Rational) => TimeDiff t where
     -- | Escape hatch; avoid.
     microseconds :: Iso' t Int64
 
--- | Convert a time difference to some 'Fractional' type.
+-- | Convert a time interval to some 'Fractional' type.
 {-# INLINE toSeconds #-}
 toSeconds :: (TimeDiff t, Fractional n) => t -> n
 toSeconds = (* recip 1000000) . fromIntegral . view microseconds
 
--- | Make a time difference from some 'Real' type.
+-- | Make a time interval from some 'Real' type.
 --
--- Where speed is a concern, make sure @n@ is one of 'Float', 'Double',
--- 'Int', 'Int64' or 'Integer', for which @RULES@ have been provided.
+-- [@Performance@] Try to make sure @n@ is one of 'Float', 'Double', 'Int',
+-- 'Int64' or 'Integer', for which rewrite @RULES@ have been provided.
 {-# INLINE fromSeconds #-}
 fromSeconds :: (Real n, TimeDiff t) => n -> t
 fromSeconds = fromSeconds' . toRational
@@ -86,6 +87,15 @@ fromSecondsIntegral _ = review microseconds . (*) 1000000 . fromIntegral
 
 ------------------------------------------------------------------------
 
+-- | An absolute time interval as measured by a clock.
+--
+-- 'DiffTime' forms an 'AdditiveGroup'―so can be added using '^+^' (or '^-^'
+-- for subtraction), and also an instance of 'VectorSpace'―so can be scaled
+-- using '*^', where
+--
+-- @
+-- type 'Scalar' 'DiffTime' = 'Rational'
+-- @
 newtype DiffTime = DiffTime Micro deriving (INSTANCES_MICRO, AdditiveGroup)
 
 #if SHOW_INTERNAL
@@ -120,6 +130,20 @@ instance TimeDiff DiffTime where
 
 ------------------------------------------------------------------------
 
+-- | A time interval as measured by UTC, that does not take leap-seconds
+-- into account.
+--
+-- For instance, the difference between @23:59:59@ and @00:00:01@ on the
+-- following day is always 2 seconds of 'NominalDiffTime', regardless of
+-- whether a leap-second took place.
+--
+-- 'NominalDiffTime' forms an 'AdditiveGroup'―so can be added using '^+^'
+-- (or '^-^' for subtraction), and also an instance of 'VectorSpace'―so can
+-- be scaled using '*^', where
+--
+-- @
+-- type 'Scalar' 'NominalDiffTime' = 'Rational'
+-- @
 newtype NominalDiffTime = NominalDiffTime Micro deriving (INSTANCES_MICRO, AdditiveGroup)
 
 #if SHOW_INTERNAL
@@ -152,15 +176,26 @@ instance TimeDiff NominalDiffTime where
     {-# INLINE microseconds #-}
     microseconds = iso (\ (NominalDiffTime (Micro u)) -> u) (NominalDiffTime . Micro)
 
+-- | The nominal length of a POSIX day: precisely 86400 SI seconds.
 {-# INLINE posixDayLength #-}
 posixDayLength :: NominalDiffTime
 posixDayLength = microseconds # 86400000000
 
 ------------------------------------------------------------------------
 
--- Represented as a 'NominalDiffTime' since MJD epoch.
+-- | The principal form of universal time, namely
+-- <http://en.wikipedia.org/wiki/Universal_Time#Versions UT1>.
+--
+-- 'UniversalTime' is defined by the rotation of the Earth around its axis
+-- relative to the Sun. Thus the length of a day by this definition varies
+-- from one to the next, and is never exactly 86400 SI seconds unlike
+-- <http://en.wikipedia.org/wiki/International_Atomic_Time TAI> or
+-- 'AbsoluteTime'. The difference between UT1 and UTC is
+-- <http://en.wikipedia.org/wiki/DUT1 DUT1>.
 newtype UniversalTime = UniversalRep NominalDiffTime deriving (INSTANCES_MICRO)
 
+-- | View 'UniversalTime' as a fractional number of days since the
+-- <http://en.wikipedia.org/wiki/Julian_day#Variants Modified Julian Date epoch>.
 {-# INLINE modJulianDate #-}
 modJulianDate :: Iso' UniversalTime Rational
 modJulianDate = iso
@@ -169,9 +204,29 @@ modJulianDate = iso
 
 ------------------------------------------------------------------------
 
--- Represented as a 'NominalDiffTime' since MJD epoch.
+-- | <http://en.wikipedia.org/wiki/Coordinated_Universal_Time Coördinated universal time>:
+-- the most common form of universal time for civil timekeeping. It is
+-- synchronised with 'AbsoluteTime' and both tick in increments of SI
+-- seconds, but UTC includes occasional leap-seconds so that it does not
+-- drift too far from 'UniversalTime'.
+--
+-- 'UTCTime' is an instance of 'AffineSpace', with
+--
+-- @
+-- type 'Diff' 'UTCTime' = 'NominalDiffTime'
+-- @
+--
+-- Use '.+^' to add (or '.-^' to subtract) time intervals of type
+-- 'NominalDiffTime', and '.-.' to get the interval between 'UTCTime's.
+--
+-- [@Performance@] Internally this is a 64-bit count of 'microseconds' since
+-- the MJD epoch, so '.+^', '.-^' and '.-.' ought to be fairly fast.
+--
+-- [@Issues@] 'UTCTime' currently
+-- <https://github.com/liyang/thyme/issues/3 cannot represent leap seconds>.
 newtype UTCTime = UTCRep NominalDiffTime deriving (INSTANCES_MICRO)
 
+-- | Unpacked 'UTCTime', partly for compatibility with @time@.
 data UTCView = UTCTime
     { utctDay :: {-# UNPACK #-}!Day
     , utctDayTime :: {-# UNPACK #-}!DiffTime
@@ -179,9 +234,11 @@ data UTCView = UTCTime
 
 instance NFData UTCView
 
+-- | 'Lens'' for the 'Day' component of an 'UTCTime'.
 _utctDay :: Lens' UTCTime Day
 _utctDay = utcTime . lens utctDay (\ (UTCTime _ t) d -> UTCTime d t)
 
+-- | 'Lens'' for the time-of-day component of an 'UTCTime'.
 _utctDayTime :: Lens' UTCTime DiffTime
 _utctDayTime = utcTime . lens utctDayTime (\ (UTCTime d _) t -> UTCTime d t)
 
@@ -192,6 +249,12 @@ instance AffineSpace UTCTime where
     {-# INLINE (.+^) #-}
     UTCRep a .+^ d = UTCRep (a ^+^ d)
 
+-- | View 'UTCTime' as an 'UTCView', comprising a 'Day' along with
+-- a 'DiffTime' offset since midnight.
+--
+-- This is an improper lens: 'utctDayTime' offsets outside the range of
+-- @['zeroV', 'posixDayLength')@ will carry over into the day part, with the
+-- expected behaviour.
 {-# INLINE utcTime #-}
 utcTime :: Iso' UTCTime UTCView
 utcTime = iso toView fromView where
