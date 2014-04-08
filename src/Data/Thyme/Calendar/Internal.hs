0c6aaab2d45af5cf61d7b3fe59fa24bdd082277b
Mihaly Barasz <klao@nilcons.com>
2014-04-08 17:45:57 +0200

    Data.Thyme.Calendar.Internal.isLeapYear: simplify and make faster
    This also makes it compile with GHC 7.8 and thus fixes #15.

--- src/Data/Thyme/Calendar/Internal.hs
+++ src/Data/Thyme/Calendar/Internal.hs
@@ -20,6 +20,7 @@ import Control.DeepSeq
 import Control.Lens
 import Control.Monad
 import Data.AffineSpace
+import Data.Bits ((.&.))
 import Data.Data
 import Data.Int
 import Data.Ix
@@ -27,10 +28,8 @@ import Data.Thyme.Format.Internal
 import Data.Vector.Unboxed (Vector)
 import qualified Data.Vector.Unboxed as V
 import GHC.Generics (Generic)
-import GHC.Prim
-import GHC.Types
 import System.Random
-import Test.QuickCheck
+import Test.QuickCheck hiding ((.&.))
 
 type Years = Int
 type Months = Int
@@ -105,13 +104,8 @@ instance NFData YearMonthDay
 
 -- | Gregorian leap year?
 isLeapYear :: Year -> Bool
-isLeapYear (I# y#) = isLeapYear# y#
-
--- | INTERNAL: avoid GHC duplicating code for each of the possibilities.
-{-# NOINLINE isLeapYear# #-}
-isLeapYear# :: Int# -> Bool
-isLeapYear# y# = remInt# y# 4# ==# 0#
-    && (remInt# y# 400# ==# 0# || remInt# y# 100# /=# 0#)
+isLeapYear y = y .&. 3 == 0 && (r100 /= 0 || q100 .&. 3 == 0) where
+    (q100, r100) = y `quotRem` 100
 
 type DayOfYear = Int
 data OrdinalDate = OrdinalDate
