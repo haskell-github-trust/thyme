bf5433800e9ae253b28c257c13536cc369b81850
Tom Mortiboy <tom.mortiboy@gmail.com>
2016-11-10 20:22:23 +0900

    Adds Control.Monad.Thyme

---
+++ src/Control/Monad/Thyme.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+#if __GLASGOW_HASKELL__ < 710
+{-# LANGUAGE OverlappingInstances #-}
+#endif
+
+module Control.Monad.Thyme
+  ( MonadTime(..)
+  ) where
+
+import Control.Monad.Trans
+import Data.Thyme
+
+-- | Class of monads which carry the notion of the current time.
+class Monad m => MonadTime m where
+  currentTime :: m UTCTime
+
+-- | Base instance for IO.
+instance {-# OVERLAPPING #-} MonadTime IO where
+  currentTime = getCurrentTime
+
+-- | Generic, overlappable instance.
+instance {-# OVERLAPPABLE #-} (MonadTime m, MonadTrans t, Monad (t m)) => MonadTime (t m) where
+  currentTime = lift currentTime
