752a5af36a186e5aef796f04601ba160b1915a09
parsonsmatt <parsonsmatt@gmail.com>
2018-06-20 21:18:09 -0600

    Parsing benchmarks

--- tests/bench.hs
+++ tests/bench.hs
@@ -1,2 +1,34 @@
+{-# LANGUAGE TypeApplications #-}
+
+module Main where
+
+import Criterion.Main
+import Control.DeepSeq (($!!))
+import qualified Data.ByteString.Char8 as BS8
+import Test.QuickCheck (arbitrary)
+import Data.Attoparsec.ByteString (parseOnly)
+
+import qualified Data.Time as Time
+import qualified Data.Thyme as Thyme
+
 main :: IO ()
-main = return ()
+main = do
+    utcthyme <- generate arbitrary :: IO Thyme.UTCTime
+    let isoFormatString = "%Y-%m-%dT%H:%M:%S%N"
+        renderIsoTime = Thyme.formatTime Thyme.defaultTimeLocale "%Y-%m-%dT%H:%M:%S%N"
+    string <- return $!! renderIsoTime utcthyme
+    bytestring <- return $!! BS8.pack (renderIsoTime utcthyme)
+
+    defaultMain
+        [ bgroup "parsing"
+            [ bench "Thyme.parseTime" $ nf
+                ((Thyme.parseTime Thyme.defaultTimeLocale isoFormatString :: String -> Maybe Thyme.UTCTime))
+                string
+            , bench "Time.parseTimeM" $ nf
+                (Time.parseTimeM True Time.defaultTimeLocale isoFormatString :: String -> Maybe Time.UTCTime)
+                string
+            , bench "Thyme.timeParser" $ nf
+                (fmap (Thyme.buildTime @Thyme.UTCTime) . parseOnly (Thyme.timeParser Thyme.defaultTimeLocale isoFormatString))
+                bytestring
+            ]
+        ]
